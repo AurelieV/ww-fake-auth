cf008ffc8ad3c82557a22a993d02ddd045f600f5
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-02-18 12:32:21 +0100

    use option api

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -8,22 +8,20 @@ import { computed, watch, onMounted } from "vue";
 export default {
     props: {
         content: { type: Object, required: true },
-        uid: { type: String, required: true },
     },
     emits: ["trigger-event"],
-    setup(props, context) {
-        const token = computed(() => props.content.token);
-        watch(
-            token,
-            (token) => {
-                console.log("watch")
-                context.emit("trigger-event", { name: "token", event: token });
-            }
-        );
-        onMounted(() => {
-            console.log("mounted")
-            context.emit("trigger-event", { name: "token", event: token });
-        })
+    computed: {
+        token() {
+            this.content.token;
+        },
+    },
+    onMounted() {
+        this.$emit("trigger-event", { name: "token", event: this.token });
+    },
+    watch: {
+        token(token) {
+            this.$emit("trigger-event", { name: "token", event: token });
+        },
     },
 };
 </script>
